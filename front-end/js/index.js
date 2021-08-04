d8f1d4930b977d15a67d438354b2ce261e38d236
Karim Cheaibi <karimc77@hotmail.fr>
2021-08-04 15:56:36 +0200

    modif index.js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,18 +1,19 @@
-fetch('http://localhost:3000/api/teddies')
-    .then((response) => response.json())
-    .then((data) => {
-        const card = document.querySelector('.peluche');
+fetch("http://localhost:3000/api/teddies")
+  .then((response) => response.json())
+  .then((data) => {
+    const card = document.querySelector(".peluche");
+    let cards = "";
+    for (const product of data) {
+      cards += `<div class="peluche" id="${product._id}">
+                    <img src="${product.imageUrl}" alt=""/> 
+                    <article class="alignprice">
+                      <h2>${product.name}</h2> 
+                      <h3>${product.price}</h3>
+                    </article>
+                </div>;`;
+    }
+    console.log(cards);
+    card.innerHTML = cards;
+  });
 
-        for (const product of data) {
-            card.innerHTML +=
-      <div class="peluche" id="${product._id}">
-        <img src="${product.imageUrl}" alt=""/> //${product.imageUrl} backend/models/teddy/js
-            <article class="alignprice">
-                <h2>${product.name}</h2> // idem recuperer le nom dans  backend/models/teddy.js
-                <h3>${product.price}</h3>     // recuperer le prix dans  backend/models/teddy.js
-            </article>
-      </div>;
-        }
-    });
-
-    console.log(card);
+//   cards.style.background ="pink";
