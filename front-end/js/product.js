ea487103321c320cabdfb9e686e7a5e9194efa74
Karim Cheaibi <karimc77@hotmail.fr>
2021-08-06 17:17:29 +0200

    Modif product.js et .html, style.css

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,28 +1,32 @@
-fetch("http://localhost:3000/api/teddies")
+const urlSite = location.hostname;
+console.log(urlSite);
+let urlApi = "http://localhost:3000/api/teddies";
+
+if(urlSite.includes('github')) {
+  urlApi = "../data/teddy.json";
+}
+
+fetch(urlApi)
   .then((response) => response.json())
   .then((data) => {
-    const card = document.querySelector(".peluche");
+    const card = document.querySelector("#article");
     let cards = "";
     
-    //   cards += `<div class="peluche" id="${product._id}">
-    //                 <img src="${product.imageUrl}" alt=""/> 
-    //                 <article class="alignprice">
-    //                   <h2>${product.name}</h2> 
-    //                   <h3>${product.price}</h3>
-    //                   <a class="product" href="panier.html" target="_blank">Ajouter au Panier</a>
-    //                 </article>
-    //             </div>`;
-
-        cards+= `
-        
-        
-        
-        
-        
-        
-        
-        `;
-    
+        cards+= `<div id="article">
+                    <h1 class="row">${card.name}</h1>
+                    <p class="row"><img src="${card.imageUrl}" alt="image d'ours en détails"/></p>
+                    <p class="row">${card.description}</p>
+                    <p class="row">Prix: ${(card.price/100).toFixed(2).replace(".",",")}€</p>
+        <!-- Personalisation de la couleur -->
+                    <label for="select__color">
+                    <h3>Personnaliser votre ours</h3>
+                    </label>
+                    <select id="${card.colors}">
+        <!-- Mes choix de couleurs --!>
+                    </select>
+                    <button class="ajoutPanier">Ajouter au panier</button>
+                </div>`;
+                
     console.log(cards);
     card.innerHTML = cards;
   });
